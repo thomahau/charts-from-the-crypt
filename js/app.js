15ea353657d0933a4b8709c9f9e9a601b4316894
Thomas Hauge <thomas.sommerfelt.hauge@gmail.com>
2018-03-20 14:03:03 +0100

    update

--- js/app.js
+++ js/app.js
@@ -16,11 +16,7 @@ const STORE = {
 
 function fetchCoinList() {
 	// Returns current top 200 cryptocurrencies by marketcap
-	const url =
-		// Bypass Same Origin Policy
-		'https://nexunity.org/tools/bypass-sop/?url=' +
-		COINMARKETCAP_ENDPOINT +
-		'ticker/?limit=200';
+	const url = COINMARKETCAP_ENDPOINT + 'ticker/?limit=200';
 
 	$.getJSON(url, populateSearchOptions).fail(showErr);
 }
